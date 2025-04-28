fd7940eda0509025bbe73065e9267003687273d7
munavvir <munavvirmuhammed296@gmail.com>
2025-04-28 18:23:50 +0530

    updated project service core policy

--- project_service/src/server.ts
+++ project_service/src/server.ts
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5002;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:3000",
+  credentials :true
+})); 
 app.use(express.json());
 
 // Routes
